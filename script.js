f84017594da7e26ec136cced9b2ab9ddb4e759cc
Minahil <113772103+MinahilMJ@users.noreply.github.com>
2026-07-21 17:28:50 +0500

    added pages for website

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<title>Minahil Mobin | AI/ML Engineer</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
+const themeToggle=document.getElementById("theme-toggle");
 
-<h1>MINAHIL MOBIN</h1>
-<p>AI/ML Engineer | Generative AI | Cybersecurity</p>
+const savedTheme=localStorage.getItem("theme");
 
-<script src="script.js"></script>
-</body>
-</html>
+if(savedTheme==="light"){
+document.body.classList.add("light-mode");
+themeToggle.textContent="☾";
+}
+
+themeToggle.addEventListener("click",()=>{
+document.body.classList.toggle("light-mode");
+
+if(document.body.classList.contains("light-mode")){
+localStorage.setItem("theme","light");
+themeToggle.textContent="☾";
+}else{
+localStorage.setItem("theme","dark");
+themeToggle.textContent="☀";
+}
+});
