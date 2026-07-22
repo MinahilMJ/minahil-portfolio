4da44403a85e2caa1e4d551f816254c60b1a26bf
Minahil <113772103+MinahilMJ@users.noreply.github.com>
2026-07-22 14:32:48 +0500

    updated for mobile screens

--- script.js
+++ script.js
@@ -35,3 +35,10 @@ sessionStorage.removeItem("scrollPosition");
 }
 });
 }
+
+const menuToggle=document.getElementById("menu-toggle");
+const navLinks=document.getElementById("nav-links");
+
+menuToggle.addEventListener("click",()=>{
+navLinks.classList.toggle("active");
+});
